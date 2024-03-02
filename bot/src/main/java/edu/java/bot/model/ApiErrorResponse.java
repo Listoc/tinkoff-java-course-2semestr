9c6f419f29247078b3048b8eef4e2f279523d4b7
Listoc <mestis0408@gmail.com>
2024-03-02 21:40:58 +0300

    Add response/request classes for bot module

---
+++ bot/src/main/java/edu/java/bot/model/ApiErrorResponse.java
@@ -0,0 +1,23 @@
+package edu.java.bot.model;
+
+public class ApiErrorResponse {
+    private final String description;
+    private final String code;
+    private final String exceptionName;
+    private final String exceptionMessage;
+    private final StackTraceElement[] stackTrace;
+
+    public ApiErrorResponse(
+        String description,
+        String code,
+        String exceptionName,
+        String exceptionMessage,
+        StackTraceElement[] stackTrace
+    ) {
+        this.description = description;
+        this.code = code;
+        this.exceptionName = exceptionName;
+        this.exceptionMessage = exceptionMessage;
+        this.stackTrace = stackTrace;
+    }
+}
