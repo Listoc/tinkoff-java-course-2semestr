661c733d6c2652dcfb4cc748167db20c2b80f8cb
Listoc <mestis0408@gmail.com>
2024-03-02 21:48:03 +0300

    Add request/response classes for botclient in scrapper module

---
+++ scrapper/src/main/java/edu/java/scrapper/model/LinkUpdateRequest.java
@@ -0,0 +1,22 @@
+package edu.java.scrapper.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.net.URI;
+
+public class LinkUpdateRequest {
+    @JsonProperty("id")
+    private final long id;
+    @JsonProperty("url")
+    private final URI url;
+    @JsonProperty("description")
+    private final String description;
+    @JsonProperty("tgChatIds")
+    private final long[] tgChatIds;
+
+    public LinkUpdateRequest(long id, URI url, String description, long[] tgChatIds) {
+        this.id = id;
+        this.url = url;
+        this.description = description;
+        this.tgChatIds = tgChatIds;
+    }
+}
