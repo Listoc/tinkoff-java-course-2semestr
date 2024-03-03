87690178be55edc977f99f6af0d8f3dee08db772
Listoc <mestis0408@gmail.com>
2024-03-03 18:49:03 +0300

    Rename TelegramChatIdHandler to ControllerHandler in scrapper module

--- scrapper/src/main/java/edu/java/scrapper/controller/ControllerHandler.java
+++ scrapper/src/main/java/edu/java/scrapper/controller/ControllerHandler.java
@@ -2,6 +2,8 @@ package edu.java.scrapper.controller;
 
 import edu.java.scrapper.exception.ChatAlreadyExistException;
 import edu.java.scrapper.exception.ChatNotExistException;
+import edu.java.scrapper.exception.LinkAlreadyExistException;
+import edu.java.scrapper.exception.LinkNotExistException;
 import edu.java.shared.model.ApiErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,7 +12,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @RestControllerAdvice
-public class TelegramChatIdHandler {
+public class ControllerHandler {
     @ExceptionHandler(ChatAlreadyExistException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ApiErrorResponse chatAlreadyExist(ChatAlreadyExistException e) {
@@ -21,6 +23,18 @@ public class TelegramChatIdHandler {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ApiErrorResponse chatNotExist(ChatNotExistException e) {
         return new ApiErrorResponse("Chat does not exist", "404", e);
+    }
+
+    @ExceptionHandler(LinkAlreadyExistException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiErrorResponse linkAlreadyExist(ChatAlreadyExistException e) {
+        return new ApiErrorResponse("Link already exist", "400", e);
+    }
+
+    @ExceptionHandler(LinkNotExistException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ApiErrorResponse linkNotExist(ChatNotExistException e) {
+        return new ApiErrorResponse("Link does not exist", "404", e);
     }
 
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
