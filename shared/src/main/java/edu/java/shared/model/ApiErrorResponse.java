fbac371b823b5fff0883f21b173e72d94ffad2f6
Listoc <mestis0408@gmail.com>
2024-03-03 17:49:20 +0300

    Add constructor with exception to ApiErrorResponse in shared module

--- shared/src/main/java/edu/java/shared/model/ApiErrorResponse.java
+++ shared/src/main/java/edu/java/shared/model/ApiErrorResponse.java
@@ -6,4 +6,8 @@ public record ApiErrorResponse(
     String exceptionName,
     String exceptionMessage,
     StackTraceElement[] stackTrace) {
+
+    public ApiErrorResponse(String description, String code, Exception e) {
+        this(description, code, e.getClass().getName(), e.getMessage(), e.getStackTrace());
+    }
 }
