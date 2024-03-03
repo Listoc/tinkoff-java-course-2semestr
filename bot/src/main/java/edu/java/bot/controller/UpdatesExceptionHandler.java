51302f33604a90f46b0199880aee12feafbe6757
Listoc <mestis0408@gmail.com>
2024-03-03 12:32:26 +0300

    Delete unused imports from UpdatesExceptionHandler in bot module

--- bot/src/main/java/edu/java/bot/controller/UpdatesExceptionHandler.java
+++ bot/src/main/java/edu/java/bot/controller/UpdatesExceptionHandler.java
@@ -2,12 +2,9 @@ package edu.java.bot.controller;
 
 import edu.java.bot.model.ApiErrorResponse;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.HttpMediaTypeNotAcceptableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.reactive.result.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
 public class UpdatesExceptionHandler {
