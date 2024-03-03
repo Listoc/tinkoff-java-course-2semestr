e5144ac654c877f9987cd6b23b443349def03fdf
Listoc <mestis0408@gmail.com>
2024-03-03 19:31:44 +0300

    Change returning ResponseEntity to adding ResponseStatus annotation

--- bot/src/main/java/edu/java/bot/controller/UpdatesController.java
+++ bot/src/main/java/edu/java/bot/controller/UpdatesController.java
@@ -3,15 +3,16 @@ package edu.java.bot.controller;
 import edu.java.shared.model.LinkUpdateRequest;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UpdatesController {
     @PostMapping("/updates")
-    public ResponseEntity<?> getUpdates(@Valid @RequestBody LinkUpdateRequest linkUpdateRequest) {
-        return ResponseEntity.status(HttpStatus.OK).body(null);
+    @ResponseStatus(HttpStatus.OK)
+    public void getUpdates(@Valid @RequestBody LinkUpdateRequest linkUpdateRequest) {
+
     }
 }
--- scrapper/src/main/java/edu/java/scrapper/controller/TelegramChatIdController.java
+++ scrapper/src/main/java/edu/java/scrapper/controller/TelegramChatIdController.java
@@ -7,10 +7,10 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,24 +18,22 @@ public class TelegramChatIdController {
     private final Map<Long, List<String>> links = new ConcurrentHashMap<>();
 
     @PostMapping("/tg-chat/{id}")
-    public ResponseEntity<?> registerChat(@PathVariable Long id) {
+    @ResponseStatus(HttpStatus.OK)
+    public void registerChat(@PathVariable Long id) {
         if (links.containsKey(id)) {
             throw new ChatAlreadyExistException("Chat has already been registered");
         }
 
         links.put(id, new ArrayList<>());
-
-        return ResponseEntity.status(HttpStatus.OK).body(null);
     }
 
     @DeleteMapping("/tg-chat/{id}")
-    public ResponseEntity<?> deleteChat(@PathVariable Long id) {
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteChat(@PathVariable Long id) {
         var result = links.remove(id);
 
         if (result == null) {
             throw new ChatNotExistException("Chat is not registered");
         }
-
-        return ResponseEntity.status(HttpStatus.OK).body(null);
     }
 }
