ae84de23f78087b4d112d1afd9bf466a3174894c
Listoc <mestis0408@gmail.com>
2024-03-03 12:31:55 +0300

    Fix UpdatesExceptionHandler in bot module

--- bot/src/main/java/edu/java/bot/controller/UpdatesExceptionHandler.java
+++ bot/src/main/java/edu/java/bot/controller/UpdatesExceptionHandler.java
@@ -3,22 +3,22 @@ package edu.java.bot.controller;
 import edu.java.bot.model.ApiErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.reactive.result.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
 public class UpdatesExceptionHandler {
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ApiErrorResponse> exceptionHandler(Exception e) {
-        return ResponseEntity
-            .status(HttpStatus.BAD_REQUEST)
-            .body(
-                new ApiErrorResponse(
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public ApiErrorResponse exceptionHandler(Exception e) {
+        return new ApiErrorResponse(
                     "Bad request parameters",
                     "400",
                     e.getClass().getName(),
                     e.getMessage(),
-                    e.getStackTrace())
-            );
+                    e.getStackTrace());
     }
 }
