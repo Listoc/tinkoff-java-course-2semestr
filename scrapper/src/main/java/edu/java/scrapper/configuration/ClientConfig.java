5167d654e6f7eb3f332ac7e79ae55ba074d3456c
Listoc <mestis0408@gmail.com>
2024-03-07 12:27:43 +0300

    Add ClientProperties in scrapper module

--- scrapper/src/main/java/edu/java/scrapper/configuration/ClientConfig.java
+++ scrapper/src/main/java/edu/java/scrapper/configuration/ClientConfig.java
@@ -2,24 +2,24 @@ package edu.java.scrapper.configuration;
 
 import edu.java.scrapper.client.GithubClient;
 import edu.java.scrapper.client.StackOverflowClient;
-import jakarta.validation.constraints.NotNull;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-@ConfigurationProperties(prefix = "app.url")
-public record ClientConfig(
-    @NotNull
-    String git,
-    @NotNull
-    String stackOverflow
-) {
+@Configuration
+public class ClientConfig {
+    private final ClientProperties clientProperties;
+
+    public ClientConfig(ClientProperties clientProperties) {
+        this.clientProperties = clientProperties;
+    }
+
     @Bean
     public GithubClient githubClient() {
-        return new GithubClient(git);
+        return new GithubClient(clientProperties.git());
     }
 
     @Bean
     public StackOverflowClient stackOverflowClient() {
-        return new StackOverflowClient(stackOverflow);
+        return new StackOverflowClient(clientProperties.stackOverflow());
     }
 }
