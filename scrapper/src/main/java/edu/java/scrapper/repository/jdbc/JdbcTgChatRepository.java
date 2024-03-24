133e91e48ae42bb00207882a399de0232e57050c
Listoc <mestis0408@gmail.com>
2024-03-24 17:40:19 +0300

    Fix DAOs in scrapper module

--- scrapper/src/main/java/edu/java/scrapper/repository/jdbc/JdbcTgChatRepository.java
+++ scrapper/src/main/java/edu/java/scrapper/repository/jdbc/JdbcTgChatRepository.java
@@ -2,6 +2,7 @@ package edu.java.scrapper.repository.jdbc;
 
 import edu.java.scrapper.model.TgChat;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class JdbcTgChatRepository {
     private final JdbcTemplate jdbcTemplate;
+    private final RowMapper<TgChat> rowMapper = getTgChatRowMapper();
 
     public JdbcTgChatRepository(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
@@ -26,26 +28,26 @@ public class JdbcTgChatRepository {
         jdbcTemplate.update(removeChat, tgChatId);
     }
 
-    public TgChat findChatById(long tgChatId) {
+    public Optional<TgChat> findChatById(long tgChatId) {
         var findChat = "SELECT * FROM chat WHERE chat_id = ?;";
-        var chatList = jdbcTemplate.query(findChat, getTgChatRowMapper(), tgChatId);
+        var chatList = jdbcTemplate.query(findChat, rowMapper, tgChatId);
 
         if (chatList.isEmpty()) {
-            return null;
+            return Optional.empty();
         }
 
-        return chatList.getFirst();
+        return Optional.of(chatList.getFirst());
     }
 
     public List<TgChat> findAllByLinkId(long linkId) {
         var findAllChats =
             "SELECT * FROM chat c JOIN chat_link_map clm ON c.chat_id = clm.chat_id WHERE clm.link_id = ?;";
-        return jdbcTemplate.query(findAllChats, getTgChatRowMapper(), linkId);
+        return jdbcTemplate.query(findAllChats, rowMapper, linkId);
     }
 
     public List<TgChat> findAll() {
         var findAllChats = "SELECT * FROM chat";
-        return jdbcTemplate.query(findAllChats, getTgChatRowMapper());
+        return jdbcTemplate.query(findAllChats, rowMapper);
     }
 
     public static RowMapper<TgChat> getTgChatRowMapper() {
