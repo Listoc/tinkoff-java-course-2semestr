016e74dbea86a7af0c1cd34ab28ea2dfa7bc0dca
Listoc <mestis0408@gmail.com>
2024-03-24 17:45:00 +0300

    Add LinkUpdater implementation in scrapper module

---
+++ scrapper/src/main/java/edu/java/scrapper/service/LinkUpdaterImpl.java
@@ -0,0 +1,62 @@
+package edu.java.scrapper.service;
+
+import edu.java.scrapper.client.BotClient;
+import edu.java.scrapper.configuration.UpdaterProperties;
+import edu.java.scrapper.model.TgChat;
+import edu.java.scrapper.service.proccesor.LinkUpdateProcessorService;
+import edu.java.shared.model.LinkUpdateRequest;
+import java.time.OffsetDateTime;
+import java.util.List;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LinkUpdaterImpl implements LinkUpdater {
+    private final LinkService linkService;
+    private final TgChatService tgChatService;
+    private final List<LinkUpdateProcessorService> linkUpdateProcessorServiceList;
+    private final UpdaterProperties updaterProperties;
+    private final BotClient botClient;
+
+    public LinkUpdaterImpl(
+        LinkService linkService,
+        TgChatService tgChatService,
+        List<LinkUpdateProcessorService> linkUpdateProcessorServiceList,
+        UpdaterProperties updaterProperties,
+        BotClient botClient
+    ) {
+        this.linkService = linkService;
+        this.tgChatService = tgChatService;
+        this.linkUpdateProcessorServiceList = linkUpdateProcessorServiceList;
+        this.updaterProperties = updaterProperties;
+        this.botClient = botClient;
+    }
+
+    public void update() {
+        var links = linkService.getLinksBeforeDateTime(OffsetDateTime.now().minus(updaterProperties.interval()));
+
+        String message = null;
+
+        for (var link : links) {
+            for (var processor : linkUpdateProcessorServiceList) {
+                message = processor.process(link);
+
+                if (message != null) {
+                    break;
+                }
+            }
+
+            if (message != null && !message.isEmpty()) {
+                botClient.sendUpdates(
+                    new LinkUpdateRequest(
+                        link.getLinkId(),
+                        link.getUrl(),
+                        message,
+                        tgChatService.getChats(link.getLinkId()).stream().map(TgChat::getChatId).toList()
+                    )
+                );
+            }
+
+            linkService.updateLastCheckTime(link.getLinkId());
+        }
+    }
+}
