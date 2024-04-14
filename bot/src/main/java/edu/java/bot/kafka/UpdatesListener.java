d18ac661d1f0adc51991426dd0c2b49ba43048b4
Listoc <mestis0408@gmail.com>
2024-04-14 21:13:39 +0300

    Add hw9

--- bot/src/main/java/edu/java/bot/kafka/UpdatesListener.java
+++ bot/src/main/java/edu/java/bot/kafka/UpdatesListener.java
@@ -2,6 +2,8 @@ package edu.java.bot.kafka;
 
 import edu.java.bot.service.UpdatesHandler;
 import edu.java.shared.model.LinkUpdateRequest;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 import jakarta.validation.Valid;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
@@ -11,13 +13,16 @@ import org.springframework.validation.annotation.Validated;
 @Validated
 public class UpdatesListener {
     private final UpdatesHandler updatesHandler;
+    private final Counter counter;
 
-    public UpdatesListener(UpdatesHandler updatesHandler) {
+    public UpdatesListener(UpdatesHandler updatesHandler, MeterRegistry meterRegistry) {
         this.updatesHandler = updatesHandler;
+        this.counter = meterRegistry.counter("messages_counter");
     }
 
     @KafkaListener(id = "1", topics = "#{@updatesTopic.name}", containerFactory = "kafkaListenerContainerFactory")
     public void listen(@Valid LinkUpdateRequest linkUpdateRequest) {
         updatesHandler.handle(linkUpdateRequest);
+        counter.increment();
     }
 }
